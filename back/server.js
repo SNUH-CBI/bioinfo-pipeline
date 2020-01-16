610d15c7e2fff7960d8415747d40c149b92f404a
server <yecnj.office@gmail.com>
2020-01-16 08:42:08 +0000

    Implementation template

--- back/server.js
+++ back/server.js
@@ -5,15 +5,17 @@ var session    = require('express-session');
 var bodyParser = require('body-parser');
 var cors       = require('cors')
 
+const config = require('./config.json');
+const server = config.server;
+
 // Middlewares
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: server.cors,
   credentials: true
 }))
 
 app.use(session({
-  // TODO: change secret-key to env
-  secret: "xcp12a!@^xvcx!@#zxc",
+  secret: server.secret_key,
   resave: false,
   saveUninitialized: true,
   cookie: {secure: false, maxAge: 60 * 60 * 1000} // expire 1 hour
@@ -28,11 +30,10 @@ app.get('/', (req, res) => {
   })
 })
 app.use('/auth', require('./api/authorization'));
-app.use('/path', require('./api/directory'));
+app.use('/directory', require('./api/directory'));
 app.use('/remove', require('./api/remove'));
 
 // Server
-var port = 4000;
-app.listen(port, function(){
-  console.log('listening on port: ' + port);
+app.listen(server.port, function(){
+  console.log('listening on port: ' + server.port);
 });
